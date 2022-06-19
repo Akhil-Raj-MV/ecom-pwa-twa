fcd6dfab5aff856fcd434277f2f7ba40875c5d7d
Akhil <108akhilraj@gmail.com>
2022-06-19 10:35:32 +0530

    service worker file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
-
+import * as serviceWorker from './serviceWorker';
 
 const root = createRoot(document.getElementById('root'));
 
@@ -11,3 +11,8 @@ root.render(
         <App />
       </BrowserRouter>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
